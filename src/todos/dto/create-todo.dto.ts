e26eb116d066a6533cf13b6b024aee079b2243f6
Ibrahim Hanif <ibrahim.hanif4529@gmail.com>
2022-02-09 09:02:39 +0700

    Add api docs in todos

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,10 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsBoolean, IsNotEmpty } from "class-validator";
 
 export class CreateTodoDto {
+    @ApiProperty()
     @IsNotEmpty()
     title: string;
+    @ApiProperty()
     @IsNotEmpty()
     description: string;
+    @ApiProperty()
     @IsNotEmpty()
     @IsBoolean()
     isDone: boolean;
