da0f7ea89b081139112c2851f891faf75d8cd34a
Ibrahim Hanif <ibrahim.hanif4529@gmail.com>
2022-02-09 08:01:14 +0700

    implement validation on todos

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,5 +1,11 @@
+import { IsBoolean, IsNotEmpty } from "class-validator";
+
 export class CreateTodoDto {
+    @IsNotEmpty()
     title: string;
+    @IsNotEmpty()
     description: string;
+    @IsNotEmpty()
+    @IsBoolean()
     isDone: boolean;
 }
